b1745431bb105eb8d858013023dfa72752372c0d
dbrisco93 <dbrisco93@gmail.com>
2020-12-17 13:41:37 -0600

    commit tree 8169758dbaec975d37c97dc5083400f3f2a8af09

--- src/components/Form.js
+++ src/components/Form.js
@@ -18,9 +18,25 @@ class Form extends React.Component {
     })
   }
 
+  handleSubmit = event => {
+    event.preventDefault()
+    let formData = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName }
+      this.sendFormDataSomewhere(form)
+    }
+
+    listOfSubmissions = () => {
+      return this.state.submittedData.map(data => {
+        return <div><span>{data.firstName}</span> <span>{data.lastName}</span></div>
+      })
+    }
+    // The above component will render previous form submissions on the page! We have a fully functioning controlled form.
+
+
   render() {
     return (
-      <form>
+      <form onSubmit={event = this.handleSubmit(event)}>
         <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
         <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
       </form>
